1c9794c9618c2f0878b86a44d6eb525b8bdd0f0e
Hannes Lerchl <hannes.lerchl@googlemail.com>
2022-02-01 21:26:56 +0100

    Refactored functions

--- key_event.c
+++ key_event.c
@@ -11,49 +11,83 @@
 #include <unistd.h>
 #include <errno.h>
 
-void poll_device(int fd, struct libevdev *dev)
+// I didn't find named constants in the headers of libevdev or linux
+// so I defined my own based on experimentation
+#define KEY_RELEASED 0
+#define KEY_PRESSED 1
+#define KEY_CONTINUED 2
+
+int handle_device_event(struct libevdev *dev)
+{
+    int rc = 0;
+    struct input_event ev;
+
+    do {
+        rc = libevdev_next_event(dev,
+                LIBEVDEV_READ_FLAG_NORMAL, &ev);
+        if (rc == 0)
+        {
+            if ((ev.type == EV_KEY) && (ev.value == KEY_RELEASED))
+            {
+                printf("Event: %s %s %d\n",
+                        libevdev_event_type_get_name(ev.type),
+                        libevdev_event_code_get_name(ev.type, ev.code),
+                        ev.value);
+            }
+        }
+    } while (rc == 1 || rc == 0);
+    return 0;
+}
+
+int read_device_event(int revents, struct libevdev *dev)
+{
+    if (revents & POLLIN)
+    {
+        return handle_device_event(dev);
+    }
+
+    switch (revents & (POLLERR | POLLRDHUP))
+    {
+        case 0:
+            printf("fds[0].revents = %i\n", revents);
+            break;
+        case POLLERR:
+            printf("Got 'POLLERR' from input device\n");
+            break;
+        case POLLRDHUP:
+            printf("Got 'POLLRDHUP' from input device\n");
+            break;
+        case POLLERR | POLLRDHUP:
+            printf("Got 'POLLERR | POLLRDHUP' from input device\n");
+            break;
+    }
+
+    return -1;
+}
+
+void poll_device(struct libevdev *dev)
 {
     int rc = 0;
     struct pollfd fds[1];
     int timeout = 5000;
-    struct input_event ev;
 
-    fds[0].fd = fd;
-    fds[0].events = POLLIN | POLLPRI | POLLRDHUP | POLLERR;
+    fds[0].fd = libevdev_get_fd(dev);
+    fds[0].events = POLLIN | POLLRDHUP | POLLERR;
+    fds[0].revents = 0;
 
     for (;;)
     {
-        fds[0].revents = 0;
         rc = poll(fds, 1, timeout);
-        printf("poll returned %i\n", rc);
         if (rc > 0)
         {
-            printf("fds[0].revents = %i\n", fds[0].revents);
-            printf("POLLIN = %i\n", POLLIN);
-            printf("POLLPRI = %i\n", POLLPRI);
-            printf("POLLOUT = %i\n", POLLOUT);
-            printf("POLLRDHUP = %i\n", POLLRDHUP);
-            printf("POLLERR = %i\n", POLLERR);
-            printf("POLLHUP = %i\n", POLLHUP);
-            printf("POLLNVAL = %i\n", POLLNVAL);
-
-            if (fds[0].revents & POLLIN)
+            if (0 != read_device_event(fds[0].revents, dev))
             {
-                do {
-                    rc = libevdev_next_event(dev,
-                            LIBEVDEV_READ_FLAG_NORMAL, &ev);
-                    if (rc == 0)
-                    {
-                        printf("Event: %s %s %d\n",
-                                libevdev_event_type_get_name(ev.type),
-                                libevdev_event_code_get_name(ev.type, ev.code),
-                                ev.value);
-                    }
-                } while (rc == 1 || rc == 0);
+                return;
             }
         }
         else
         {
+            printf("poll returned %i\n", rc);
             return;
         }
     }
@@ -72,25 +106,56 @@ void print_device_info(const char *device, struct libevdev *dev)
     printf("Driver version '%i'\n", libevdev_get_driver_version(dev));
 }
 
-int main(int argc, char* argv[]) {
-    struct libevdev *dev = NULL;
-    int fd = -1;
-    int rc = 1;
-    const char* device = "/dev/input/event0";
+struct libevdev* init_device(const char* device_name)
+{
+    struct libevdev* dev = NULL;
 
+    const int fd = open(device_name, O_RDONLY | O_NONBLOCK);
+    if (fd < 0)
+    {
+        fprintf(stderr, "Failed to open '%s'\n", device_name);
+        return NULL;
+    }
+
+    const int rc = libevdev_new_from_fd(fd, &dev);
+    if (rc < 0)
+    {
+        close(fd);
+        fprintf(stderr, "Failed to init libevdev (%s)\n", strerror(-rc));
+        return NULL;
+    }
+
+    return dev;
+}
+
+void deinit_device(struct libevdev *dev)
+{
+    const int fd = libevdev_get_fd(dev);
+
+    libevdev_free(dev);
+    close(fd);
+}
+
+const char* get_device_name(int argc, char *argv[])
+{
     if (argc > 1)
     {
-        device = argv[1];
+        return argv[1];
     }
-
-    fd = open(device, O_RDONLY | O_NONBLOCK);
-    rc = libevdev_new_from_fd(fd, &dev);
-    if (rc < 0 || fd < 0) {
-        fprintf(stderr, "Failed to init libevdev (%s)\n", strerror(-rc));
-        exit(1);
+    else
+    {
+        return "/dev/input/event0";
     }
+}
 
-    print_device_info(device, dev);
-    poll_device(fd, dev);
-    close(fd);
+int main(int argc, char* argv[]) {
+    const char* device = get_device_name(argc, argv);
+    struct libevdev *dev = init_device(device);
+
+    if (dev != NULL)
+    {
+        print_device_info(device, dev);
+        poll_device(dev);
+        deinit_device(dev);
+    }
 }
