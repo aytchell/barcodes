454a5e5c93f2542f4b967e2a7b73efe626b4752d
Hannes Lerchl <hannes.lerchl@googlemail.com>
2022-02-04 10:26:38 +0100

    Added a logging facade

---
+++ src/logger.c
@@ -0,0 +1,50 @@
+#include "logger.h"
+#include <stdio.h>
+#include <stdarg.h>
+
+#define MAX_LOG_MSG_LEN     256
+
+int logger_init(
+        __attribute__((unused)) const struct config *config)
+{
+    return TRUE;
+}
+
+static const char* level_name(int prio)
+{
+    switch (prio)
+    {
+        case LOG_EMERG:     return "EMRG";
+        case LOG_ALERT:     return "ALRT";
+        case LOG_CRIT:      return "CRIT";
+        case LOG_ERR:       return "ERR ";
+        case LOG_WARNING:   return "WARN";
+        case LOG_NOTICE:    return "NOTE";
+        case LOG_INFO:      return "INFO";
+        case LOG_DEBUG:     return "DEBG";
+        default:            return "????";
+    }
+}
+
+void logger_log(int prio, const char *format, ...)
+{
+    char message[MAX_LOG_MSG_LEN];
+    va_list args;
+
+    va_start(args, format);
+    vsnprintf(message, MAX_LOG_MSG_LEN, format, args);
+    va_end(args);
+
+    if (prio >= LOG_WARNING)
+    {
+        fprintf(stderr, "%s - %s\n", level_name(prio), message);
+    }
+    else
+    {
+        fprintf(stdout, "%s - %s\n", level_name(prio), message);
+    }
+}
+
+void logger_deinit()
+{
+}
---
+++ src/logger.h
@@ -0,0 +1,22 @@
+#ifndef LOGGER_H
+#define LOGGER_H
+
+#include "config.h"
+
+#define LOG_EMERG   0   /* system is unusable */
+#define LOG_ALERT   1   /* action must be taken immediately */
+#define LOG_CRIT    2   /* critical conditions */
+#define LOG_ERR     3   /* error conditions */
+#define LOG_WARNING 4   /* warning conditions */
+#define LOG_NOTICE  5   /* normal but significant condition */
+#define LOG_INFO    6   /* informational */
+#define LOG_DEBUG   7   /* debug-level messages */
+
+
+int logger_init(const struct config *config);
+
+void logger_log(int prio, const char *format, ...);
+
+void logger_deinit();
+
+#endif // LOGGER_H
