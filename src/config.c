aace42b48ec232464062e177f3ad95f732f06871
Hannes Lerchl <hannes.lerchl@googlemail.com>
2022-02-08 11:43:18 +0100

    First version to read config from file

--- src/config.c
+++ src/config.c
@@ -2,16 +2,18 @@
 #include "logger.h"
 #include <string.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
 
-#define SCANNER_VENDOR_ID   0x28e9
-#define SCANNER_PRODUCT_ID  0x03d9
+#define SCANNER_VENDOR_ID   -1
+#define SCANNER_PRODUCT_ID  -1
 
-#define NONPRIV_GROUP_ID    1000
-#define NONPRIV_USER_ID     1000
+#define NONPRIV_GROUP_ID    65534
+#define NONPRIV_USER_ID     65534
 
 #define NEVER_TIMEOUT       -1
-#define JAMBEL_EVENT_URL    "http://localhost:8080/event-input/558a4918-54a7-492c-b268-3790f4d5f0f5"
-#define HTTP_CONTENT_TYPE   "application/vnd.com.github.aytchell.eventvalue-v1+json"
+#define JAMBEL_EVENT_URL    "http://localhost:8080/barcode"
+#define HTTP_CONTENT_TYPE   "application/json"
 
 #define MAX_CONFIG_LINE_LEN 768
 
@@ -19,12 +21,12 @@ void set_defaults(struct config *config)
 {
     config->scanner_vendor_id = SCANNER_VENDOR_ID;
     config->scanner_product_id = SCANNER_PRODUCT_ID;
-    config->nonpriv_gid = NONPRIV_GROUP_ID;
     config->nonpriv_uid = NONPRIV_USER_ID;
+    config->nonpriv_gid = NONPRIV_GROUP_ID;
     config->scan_timeout = NEVER_TIMEOUT;
     config->log_threshold = LOG_DEBUG;
     config->log_to_syslog = FALSE;
-    strncpy(config->http_upload_verb, "PUT", MAX_UPLOAD_VERB_LEN);
+    strncpy(config->http_upload_verb, "POST", MAX_UPLOAD_VERB_LEN);
     strncpy(config->http_target_url, JAMBEL_EVENT_URL, MAX_TARGET_URL_LEN);
     strncpy(config->http_content_type, HTTP_CONTENT_TYPE, MAX_CONTENT_TYPE_LEN);
     strncpy(config->http_json_payload_name, "payload", MAX_PAYLOAD_NAME_LEN);
@@ -77,11 +79,186 @@ static char* find_last_not_of(char *line, char *patterns)
     return last;
 }
 
-static int process_param(const char* key, const char *value,
-        __attribute__((unused)) struct config *config)
+static int parse_int(const char *value)
 {
-    fprintf(stdout, "Key: '%s' - Value: '%s'\n", key, value);
+    char *endptr = NULL;
+    int base = 10;
+    if (value[0] == '0' && (value[1] == 'x' || value[1] == 'X'))
+    {
+        base = 16;
+    }
+
+    long val = strtol(value, &endptr, base);
+    if (*endptr != '\0') return FALSE;
+    if (val > SHRT_MAX) return FALSE;
+    if (val < 0) return FALSE;
+    return val;
+}
+
+static int process_vendor_id(const char *value, struct config *config)
+{
+    const int int_val = parse_int(value);
+    if (int_val == -1)
+        return FALSE;
+    config->scanner_vendor_id = int_val;
     return TRUE;
+}
+
+static int process_product_id(const char *value, struct config *config)
+{
+    const int int_val = parse_int(value);
+    if (int_val == -1)
+        return FALSE;
+    config->scanner_product_id = int_val;
+    return TRUE;
+}
+
+static int process_uid(const char *value, struct config *config)
+{
+    const int int_val = parse_int(value);
+    if (int_val == -1)
+        return FALSE;
+    config->nonpriv_uid = int_val;
+    return TRUE;
+}
+
+static int process_gid(const char *value, struct config *config)
+{
+    const int int_val = parse_int(value);
+    if (int_val == -1)
+        return FALSE;
+    config->nonpriv_gid = int_val;
+    return TRUE;
+}
+
+static int process_log_threshold(const char *value, struct config *config)
+{
+    if (0 == strcasecmp("CRITICAL", value))
+    {
+        config->log_threshold = LOG_CRIT;
+        return TRUE;
+    }
+
+    if (0 == strcasecmp("ERROR", value))
+    {
+        config->log_threshold = LOG_ERR;
+        return TRUE;
+    }
+
+    if (0 == strcasecmp("WARNING", value))
+    {
+        config->log_threshold = LOG_WARNING;
+        return TRUE;
+    }
+
+    if (0 == strcasecmp("NOTICE", value))
+    {
+        config->log_threshold = LOG_NOTICE;
+        return TRUE;
+    }
+
+    if (0 == strcasecmp("INFO", value))
+    {
+        config->log_threshold = LOG_INFO;
+        return TRUE;
+    }
+
+    if (0 == strcasecmp("DEBUG", value))
+    {
+        config->log_threshold = LOG_DEBUG;
+        return TRUE;
+    }
+
+    return FALSE;
+}
+
+static int process_use_syslog(const char *value, struct config *config)
+{
+    if (
+            (0 == strcasecmp("TRUE", value)) ||
+            (0 == strcasecmp("YES", value)))
+    {
+        config->log_to_syslog = TRUE;
+        return TRUE;
+    }
+
+    if (
+            (0 == strcasecmp("FALSE", value)) ||
+            (0 == strcasecmp("NO", value)))
+    {
+        config->log_to_syslog = FALSE;
+        return TRUE;
+    }
+
+    return FALSE;
+}
+
+static int process_http_upload_verb(const char *value, struct config *config)
+{
+    if (
+            (0 == strcmp("PUT", value)) ||
+            (0 == strcmp("POST", value)) ||
+            (0 == strcmp("PATCH", value)))
+    {
+        strncpy(config->http_upload_verb, value, MAX_UPLOAD_VERB_LEN);
+        return TRUE;
+    }
+
+    return FALSE;
+}
+
+static int process_http_target_url(const char *value, struct config *config)
+{
+    strncpy(config->http_target_url, value, MAX_TARGET_URL_LEN);
+    return TRUE;
+}
+
+static int process_http_content_type(const char *value, struct config *config)
+{
+    strncpy(config->http_content_type, value, MAX_CONTENT_TYPE_LEN);
+    return TRUE;
+}
+
+static int process_http_json_payload_name(const char *value, struct config *config)
+{
+    strncpy(config->http_json_payload_name, value, MAX_PAYLOAD_NAME_LEN);
+    return TRUE;
+}
+
+static int process_param(
+        const char* key, const char *value, struct config *config)
+{
+    if (0 == strcmp("vendor_id", key))
+        return process_vendor_id(value, config);
+
+    if (0 == strcmp("product_id", key))
+        return process_product_id(value, config);
+
+    if (0 == strcmp("uid", key))
+        return process_uid(value, config);
+
+    if (0 == strcmp("gid", key))
+        return process_gid(value, config);
+
+    if (0 == strcmp("log_threshold", key))
+        return process_log_threshold(value, config);
+
+    if (0 == strcmp("use_syslog", key))
+        return process_use_syslog(value, config);
+
+    if (0 == strcmp("http_upload_verb", key))
+        return process_http_upload_verb(value, config);
+
+    if (0 == strcmp("http_target_url", key))
+        return process_http_target_url(value, config);
+
+    if (0 == strcmp("http_content_type", key))
+        return process_http_content_type(value, config);
+
+    if (0 == strcmp("http_json_payload_name", key))
+        return process_http_json_payload_name(value, config);
+
+    return FALSE;
 }
 
 const char *trim(char* string)
